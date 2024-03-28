96e2cdf4c721e1edb7d1b62e79e9b70739544fc3
Prerak1310 <preraksangwan@gmail.com>
2024-03-28 11:52:45 +0530

    added more doctors

--- frontend/src/components/doctorcomp.js
+++ frontend/src/components/doctorcomp.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 export default function Doctorcomp({ imgurl, docname, post, docprofile }) {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-black bg-white bg-opacity-40 p-3 shadow-lg shadow-black ">
       {/* about the doctors */}
 
       <div>
-        <img alt="alt text" className="w-[100px] rounded-full" src={imgurl} />
+        <img
+          alt="alt text"
+          className="h-[100px] w-[100px] rounded-full"
+          src={imgurl}
+        />
       </div>
       <div className="font-bold text-black">{docname}</div>
       <div className="mb-3">{post}</div>
@@ -16,7 +21,10 @@ export default function Doctorcomp({ imgurl, docname, post, docprofile }) {
         <button className="w-full rounded-md bg-gradient-to-r from-amber-400 via-amber-500 to-amber-400 p-2 text-center hover:bg-gradient-to-r hover:from-amber-500 hover:via-amber-600 hover:to-amber-500">
           View Profile
         </button>
-        <button className="w-full rounded-md bg-gradient-to-r from-amber-400 via-amber-500 to-amber-400 p-2 text-center hover:bg-gradient-to-r hover:from-amber-500 hover:via-amber-600 hover:to-amber-500">
+        <button
+          onClick={() => navigate("/appointments")}
+          className="w-full rounded-md bg-gradient-to-r from-amber-400 via-amber-500 to-amber-400 p-2 text-center hover:bg-gradient-to-r hover:from-amber-500 hover:via-amber-600 hover:to-amber-500"
+        >
           Book an appointment
         </button>
       </div>
--- frontend/src/pages/doctors.js
+++ frontend/src/pages/doctors.js
@@ -10,12 +10,36 @@ export default function Doctors() {
         docprofile="https://www.fortishealthcare.com/doctors/dr-anoop-misra-2205"
       />
       <Doctorcomp
-        imgurl={require("../assets/doctors/anup.jpg")}
-        docname="Dr. Ajay Kaul"
+        imgurl={require("../assets/doctors/doctor2.jpeg")}
+        docname="Dr. Ajay Sharma"
         post="CHAIRMAN CARDIAC SCIENCE | Fortis"
         docprofile="https://www.fortishealthcare.com/doctors/dr-ajay-kaul-6323"
       />
-      <Doctorcomp />
+      <Doctorcomp
+        imgurl={require("../assets/doctors/doctor1.jpeg")}
+        docname="Dr. Pooja Kaul"
+        post="CHAIRMAN CARDIAC SCIENCE | Fortis"
+        docprofile="https://www.fortishealthcare.com/doctors/dr-ajay-kaul-6323"
+      />
+      <Doctorcomp
+        imgurl={require("../assets/doctors/doctor3.jpeg")}
+        docname="Dr. Seema Kataria"
+        post="CHAIRMAN CARDIAC SCIENCE | Fortis"
+        docprofile="https://www.fortishealthcare.com/doctors/dr-ajay-kaul-6323"
+      />
+      <Doctorcomp
+        imgurl={require("../assets/doctors/doctor4.jpeg")}
+        docname="Dr. Deepak Sharma"
+        post="CHAIRMAN CARDIAC SCIENCE | Fortis"
+        docprofile="https://www.fortishealthcare.com/doctors/dr-ajay-kaul-6323"
+      />
+
+      <Doctorcomp
+        imgurl={require("../assets/doctors/doctor5.jpeg")}
+        docname="Dr. Kartik Jha"
+        post="CHAIRMAN CARDIAC SCIENCE | Fortis"
+        docprofile="https://www.fortishealthcare.com/doctors/dr-ajay-kaul-6323"
+      />
     </div>
   );
 }
