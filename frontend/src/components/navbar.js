268533fcb00c6a9863a056b67862925dc17d5715
Prerak1310 <preraksangwan@gmail.com>
2024-03-28 12:22:37 +0530

    textbooks

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -6,10 +6,13 @@ export default function Navbar() {
     <div>
       <div className=" grid grid-flow-row items-center gap-y-2 bg-[#2c2929] p-4 text-white  xl:grid xl:grid-flow-col xl:justify-between">
         <div
+          className="grid grid-flow-row items-center justify-center gap-y-2 text-center"
           onClick={() => navigate("/")}
-          className="text-center text-4xl font-bold hover:cursor-pointer xl:text-5xl xl:hover:text-yellow-400"
         >
-          Light Loom<span className="text-red-600">.</span>
+          <div className="text-center text-4xl font-bold hover:cursor-pointer xl:text-5xl xl:hover:text-yellow-400">
+            Light Loom<span className="text-red-600">.</span>
+          </div>
+          <div className="font-bold italic">Beyond textbook and taboos</div>
         </div>
         <div className="grid grid-flow-col  justify-center gap-x-3 xl:grid xl:grid-flow-col xl:gap-x-4">
           <button
